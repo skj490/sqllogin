01d625f0311026d71e9ce23fd984293d3f9535ce
skj490 <ser5@njit.edu>
2014-02-15 10:32:16 -0500

    Create sqllogin.php
    sqllogin script to be called by curl

---
+++ sqllogin.php
@@ -0,0 +1,27 @@
+<?php
+	ini_set('display_errors',1); 
+	error_reporting(E_ALL);
+	session_start();
+	$sql=mysqli_connect("sql.njit.edu","ser5","rTlzzhFwz","ser5");
+
+	if(mysqli_connect_errno($sql))
+	{
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+
+	$login=mysqli_query($sql, "SELECT * FROM USER WHERE UNAME='".$_POST["uname"]."'");
+
+	$data=mysqli_fetch_array($login);
+	$password=$_POST["passwd"];
+	$password = md5($password);
+	if($_POST["passwd"] != "" && $password===$data['PASSWD'])
+	{
+		$_SESSION['uname']=$_POST["uname"];
+		echo json_encode("Valid SQL login<br>");
+	}
+	else
+	{
+		echo json_encode("Wrong SQL Database Password<br>");
+	}
+
+?>
