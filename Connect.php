407749380012cdf98998d548cfa8d6cdd98b17f8
skj490 <ser5@njit.edu>
2014-02-16 18:57:01 -0500

    Create Connect.php

---
+++ Connect.php
@@ -0,0 +1,31 @@
+<?php
+//Code will send data to Njit_Auth.php for user authentication
+
+	
+	$Username =$_POST['username'];
+	$Password =$_POST['password'];
+	
+	$crl=curl_init();
+	curl_setopt($crl, CURLOPT_URL,"http://web.njit.edu/~krp56/njit_auth.php");
+	curl_setopt($crl, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($crl, CURLOPT_POSTFIELDS, "username=$Username&password=$Password");
+	curl_setopt($crl, CURLOPT_FOLLOWLOCATION, 1);
+	
+	$result= curl_exec($crl);
+	//check result
+	
+	curl_close($crl);
+	return $result;
+	
+	/* $result = json_decode($result);
+	
+	if($result == "true"){
+	header("location: loginSucc.html");
+	}
+	else{
+	header("location:index.php?error=inc1");
+	} */
+
+
+?>
