145616cb7395e0aeeaa701d932a7359e57f3ec2c
skj490 <ser5@njit.edu>
2014-02-16 12:55:12 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,25 +1,37 @@
 <?php
 
 function sql_login($user, $pass){
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, "http://afsaccess1.njit.edu/~ser5/sqllogin.php");
-	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
+	$ch = curl_init();	
+	//URL for Authentication by Sam in Back end	
+	curl_setopt($ch, CURLOPT_URL, "http://afsaccess1.njit.edu/~ser5/sqllogin.php"); 
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
 	 	"uname" => $user,
 	 	"passwd" => $pass,
 	)));
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	$result = curl_exec($ch);
 	curl_close($ch);
-
 	return $result;
 }
 
 ini_set('display_errors',1); 
  error_reporting(E_ALL);
 $result = sql_login($_POST["uname"],$_POST["passwd"]);
-//echo "$result<br>";
 $result = json_decode($result);
-echo "$result<br>";
+
+if($result == "true"){ //This can be updated if needed, but just checks the output of sqllogin.php which is true if password is correct
+	header("location: loginSucc.html"); //Switch to display login successful 
+	}
+	else{
+	echo 'Incorrect UCID or Password'; // Send back message to jscript and display on same screen
+	}
+	
+	
+	
+	
+	
+	
+
 /*if ( $UCID == '1' )
 {
 	echo "Valid UCID login<br>";
