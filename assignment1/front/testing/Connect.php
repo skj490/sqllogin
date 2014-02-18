f099b8d774ba6fb0dc9b54813c2518d6fce02c97
skj490 <ser5@njit.edu>
2014-02-18 12:09:30 -0500

    Update Connect.php

--- assignment1/front/testing/Connect.php
+++ assignment1/front/testing/Connect.php
@@ -9,6 +9,7 @@ ini_set('display_errors',1);
 	$data_string = json_encode($data);  
 	$crl=curl_init();
 	curl_setopt($crl, CURLOPT_URL,"http://web.njit.edu/~jeb26/cs490/login_system/auth.php");
+	//curl_setopt($crl, CURLOPT_URL,"http://web.njit.edu/~ser5/auth.php");
 	//curl_setopt($crl, CURLOPT_POST, 1);
 	curl_setopt($crl, CURLOPT_POSTFIELDS, $data_string );
 	curl_setopt($crl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
@@ -19,16 +20,24 @@ ini_set('display_errors',1);
 	
 	curl_close($crl);
 	$result = json_decode($result, true);
+	var_dump($result);
+	
 	if ( $result["authNJIT"] && $result["AuthLocal"] )
 	{
-		header("location: loginUnSucc.html");
+		header("location: loginSucc.html");
 	}
 	else if ( $result["authNJIT"] )
 	{
-		header("location: ucidSucc.html");
+		header("location: njitSucc.html");
 	}
-	else 
+	else if ( $result["AuthLocal"] )
 	{
 		header("location: sqlSucc.html");
 	}
+	else
+	{
+		header("location: loginUnSucc.html");
+	}
+	
 ?>
+	
