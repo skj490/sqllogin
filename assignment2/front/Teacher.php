dce07deed9f6bc61e58e224023abd76bc309e877
skj490 <ser5@njit.edu>
2014-03-11 06:21:21 -0400

    Create Teacher.php

---
+++ assignment2/front/Teacher.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>Teacher Section</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/style1.css">
+</head>
+<body>
+  <section class="container">
+    <nav>
+      <ul class="nav">      
+        <li class="active"><a href="Teacher.php" title="Home">Home</a></li>
+        <!--<li><a href="ListQ.php" title="TestBank">Test Bank</a></li>-->
+        <li><a href="MakeQues.php" title="Add question">Create Question</a></li>
+        <li><a href="CreateEx.php" title="Create Exam">Create Exam</a></li>        
+        <li><a href="ListEx.php" title="Show Exam">Show Exams</a></li>
+        <li><a href="about.php" title="About">About</a></li>
+        <li><a href="index.html" title="Logout">Log-out</a></li>
+      </ul>
+    </nav>
+  </section> 
+    
+  <section class="about">
+  <div id="author">
+  		<p><span style="font-size: 18px">Welcome Professor</span></p> <span style="font-size: 18px">What Would You Like To DO?</span>
+   	<input type="hidden">
+     </form>
+     </div>
+     </section>
+</body>
+</html>
