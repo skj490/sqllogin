d19dd956d5b1cf627074ee2394ec1d84dd1cc8d9
skj490 <ser5@njit.edu>
2014-03-11 06:40:11 -0400

    Create about2.php

---
+++ assignment2/front/about2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="css/style.css">
+ 	<title> Student Section </title>
+</head>
+<body>  <section class="container">
+    <nav>
+      <ul class="nav">      
+        <li ><a href="Student.php" title="Home">Home</a></li>
+        <li><a href="ShowExams.php" title="TestBank">Exams Due</a></li>
+        <li><a href="ShowGrades.php" title="My Grades">My Grades</a></li>
+        <li class="active"><a href="about2.php" title="About">About</a></li>
+        <li><a href="index.html" title="Loggout">Loggout</a></li>
+      </ul>
+    </nav>
+  </section>
+  <section class="about">
+    <p class="about-author">
+      Login Form Created By: <br> Kishan Patel (Front End)<br /> James Bell (Middle End) <br /> Samuel Roberts (Back End) <br>
+      CS 490 Online Examination project
+    </p>
+  </section>
+</body>
+</html>
