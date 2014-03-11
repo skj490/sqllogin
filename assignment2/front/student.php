9a77891d0a8c4c44bb17bb81f32256ff58d6ef00
skj490 <ser5@njit.edu>
2014-03-11 06:41:06 -0400

    Create student.php

---
+++ assignment2/front/student.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>Student Section</title>
+  <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="css/style1.css">
+</head>
+<body>
+  <section class="container">
+    <nav>
+      <ul class="nav">      
+        <li class="active"><a href="Student.php" title="Home">Home</a></li>
+        <li><a href="ShowExams.php" title="TestBank">Exams Due</a></li>
+        <li><a href="ShowGrades.php" title="My Grades">My Grades</a></li>
+        <li><a href="about2.php" title="About">About</a></li>
+        <li><a href="index.html" title="Log out">Loggout</a></li>
+      </ul>
+    </nav>
+  </section> 
+  <section class="about">
+  <div id="author">
+  		<p><span style="font-size: 18px">Welcome To CS 490 Exam Center</span></p> <span style="font-size: 18px">What Would You Like To DO?</span>
+   	<input type="hidden">
+     </form>
+     </div>
+     </section>
+</body>
+</html>
